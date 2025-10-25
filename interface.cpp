ba975808f09c578db2f8e2e8359c5f51cbb6a43e
Sambhav Singh Aditya <sambhavsinghaditya@Sambhavs-Laptop-6.local>
2025-10-25 13:46:26 +0530

    cleaned and added expected output

--- interface.cpp
+++ interface.cpp
@@ -110,12 +110,12 @@ void getcommand(string line){
 }
 
 int main(){
-    freopen("executables/input.txt","r",stdin);
+    //freopen("executables/input.txt","r",stdin);
     string line;
     currUserCount=0;
     cout<<"> ";
     while(getline(cin, line)){
-        cout<<line<<endl;
+        //cout<<line<<endl;
         if(line=="EXIT"){
             break;
         }
